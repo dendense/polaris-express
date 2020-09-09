2a3b75d6990faee70cfb1c024f7e8c7a2682a77c
fakhrads <fakhriadisaputra15@gmail.com>
2020-09-09 10:08:17 +0700

    Add Route for post

---
+++ app/routes/post.routes.js
@@ -0,0 +1,20 @@
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/post.controller");
+
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+  
+  app.post(
+    "/api/post/create",
+    [authJwt.verifyToken],
+    controller.create
+  );
+
+};
